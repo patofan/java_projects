9ae4f449c1bae5de42b5d2c985a076f9c8ec81ed
patofan <patofan@gmail.com>
2015-06-13 18:18:50 +0800

    1.0

---
+++ helloworld1/src/main/java/helloworld1/HelloWorld.java
@@ -0,0 +1,15 @@
+package helloworld1;
+
+public class HelloWorld {
+   
+	public void print() {
+		System.out.println( "Hi");
+	}
+	
+	public void main( String [] args ){
+		HelloWorld m = new HelloWorld();
+		m.print();
+	}
+	
+	
+}
