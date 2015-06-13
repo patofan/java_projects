e7eb6c36d211d9dd6dbe193ec727979ba9bbc81d
patofan <patofan@gmail.com>
2015-06-13 16:00:08 +0800

    1.0 first commit

---
+++ Hello1/src/patofan/HelloWorld1.java
@@ -0,0 +1,15 @@
+package patofan;
+
+public class HelloWorld1 {
+
+	public void sayHello() {
+		System.out.println( "Hello world 1");
+	}
+	
+	
+	public static void main(String[] args) {
+		HelloWorld1 m = new HelloWorld1();
+		m.sayHello();
+	}
+
+}
